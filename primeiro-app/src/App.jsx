21e8bd0dac0348510046e0c03a19980230acdc44
Rodrigo Bossini <professorbossini@gmail.com>
2025-02-14 18:21:37 -0300

    learn(ap03-react): encerra o codigo 2.7.2
    - retomar fazendo o codigo 2.7.3
    - lembrar que tivemos problemas com create-react-app e substituimos por vite

---
+++ primeiro-app/src/App.jsx
@@ -0,0 +1,23 @@
+function App () {
+  return(
+    <div
+      style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8 }}>
+        <label 
+          htmlFor="nome"
+          style={{display: 'block', marginBottom: 4}}>
+            Nome:
+        </label>
+        <input 
+          type="text" 
+          id="nome"
+          style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none'}} />
+        <button
+          style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}}>
+          Enviar
+        </button>
+    </div>
+  )
+}
+
+export default App
+
