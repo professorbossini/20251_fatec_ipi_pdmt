f9b5ff2d523a68b2d81f4f8a1c8175cc51ee1fd6
Rodrigo Bossini <professorbossini@gmail.com>
2025-02-21 17:35:46 -0300

    learn(ap03-react): encerra a apostila

--- primeiro-app/src/App.jsx
+++ primeiro-app/src/App.jsx
@@ -1,19 +1,26 @@
+import './styles.css'
 function App () {
+  const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}
+  const textoDoRotulo = 'Nome:'
+  const obterTextoDoBotao = () => 'Enviar'
+  const aoClicarNoBotao = () => alert('Clicou')
   return(
     <div
       style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8 }}>
         <label 
           htmlFor="nome"
+          className='rotulo'
           style={{display: 'block', marginBottom: 4}}>
-            Nome:
+            {textoDoRotulo}
         </label>
         <input 
           type="text" 
           id="nome"
           style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none'}} />
         <button
-          style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}}>
-          Enviar
+          onClick={() => aoClicarNoBotao()}
+          style={estilosBotao}>
+          {obterTextoDoBotao()}
         </button>
     </div>
   )
