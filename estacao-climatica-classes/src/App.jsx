77fc02dc011c06f13479592c1f4484f21a9a74ce
Rodrigo Bossini <professorbossini@gmail.com>
2025-03-07 17:44:41 -0300

    learn(ap05-react): encerra a secao 2.8

--- estacao-climatica-classes/src/App.jsx
+++ estacao-climatica-classes/src/App.jsx
@@ -7,9 +7,58 @@ class App extends React.Component{
       longitude: null,
       estacao: null,
       data: null,
-      icona: null
+      icone: null
     }
+  }
 
+  obterEstacao = (data, latitude) => {
+    const anoAtual = data.getFullYear()
+    //21/06
+    const d1 = new Date(anoAtual, 5, 21)
+    //24/09
+    const d2 = new Date(anoAtual, 8, 24)
+    //22/12
+    const d3 = new Date(anoAtual, 11, 22)
+    //21/03
+    const d4 = new Date(anoAtual, 2, 21)
+    const estaNoSul = latitude < 0
+    if(data >= d1 && data < d2)
+      return estaNoSul ? 'Inverno' : 'Verão'
+    if(data >= d2 && data < d3)
+      return estaNoSul ? 'Primavera' : 'Outono'
+    if (data >= d3 || data < d4)
+      return estaNoSul ? 'Verão' : 'Inverno'
+    return estaNoSul ? 'Outono' : 'Primavera'
+  }
+
+  icones = {
+    'Primavera': 'seedling',
+    'Verão': 'clouds-sun',
+    'Outono': 'tree',
+    'Inverno': 'snowflake'
+  }
+
+  obterLocalizacao = () => {
+    navigator.geolocation.getCurrentPosition(
+      (posicao) => {
+        const dataAtual = new Date()
+        const estacao = this.obterEstacao(dataAtual, posicao.coords.latitude)
+        const icone = this.icones[estacao]
+        // não faça assim, não atualiza a tela, embora atualize a variável
+        // this.state.latitude = posicao.coords.latitude
+        //faça assim :)
+        this.setState({
+          latitude: posicao.coords.latitude,
+          longitude: posicao.coords.longitude,
+          estacao: estacao,
+          data: dataAtual.toLocaleTimeString(),
+          icone: icone
+        })
+      },
+      (erro) => {
+
+      }
+    )
   }
   render(){
     return(
