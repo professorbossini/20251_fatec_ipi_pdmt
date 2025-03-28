d20eda39c44faaa47168a9c276aecbd1faa8a1b1
Rodrigo Bossini <professorbossini@gmail.com>
2025-03-28 17:01:21 -0300

    learn(ap07-react): encerra a pagina 17
    - fizemos toda a configuracao para usar o react-dotenv
    - foi necessario ajustar o index.html manualmente
    - a seguir, testar se esta funcionando
    - lembrando que o pacote react-dotenv foi projetado visando aplicacoes criadas com CRA

--- exibe-fotos/src/components/App.jsx
+++ exibe-fotos/src/components/App.jsx
@@ -2,13 +2,26 @@
 import React from 'react'
 import Busca from './Busca'
 
-const App = () => {
-  return (
-    <div>
-        <h1>Exibir uma lista de...</h1>
-        <Busca />
-    </div>
-  )
+class App extends React.Component {
+
+  onBuscaRealizada = (termo) => {
+    console.log(termo)
+  }
+
+  render(){
+    return (
+      <div className='grid justify-content-center'>
+          <div className="col-12">
+            <h1 className='text-center'>Exibir uma lista de...</h1>
+          </div>
+          <div className="col-12">
+            <Busca 
+              dica="Digite algo que deseja ver..."
+              onBuscaRealizada={this.onBuscaRealizada}/>
+          </div>
+      </div>
+    )
+  }
 }
 
 export default App
--- exibe-fotos/src/components/Busca.jsx
+++ exibe-fotos/src/components/Busca.jsx
@@ -1,24 +1,57 @@
 // rce
 import React, { Component } from 'react'
+import { Button } from 'primereact/button'
 import { IconField } from 'primereact/iconfield'
 import { InputIcon } from 'primereact/inputicon'
 import { InputText } from 'primereact/inputtext'
 export class Busca extends Component {
+    
     state = {
         termoDeBusca: ''
     }
-    
+
+    onTermoAlterado = (event) => {
+        console.log(event.target.value)
+        //atualizar o estado do componente
+        //o termoDeBusca deve ser igual ao texto que existe na propriedade value do target do event
+        this.setState({
+            termoDeBusca: event.target.value
+        })
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault()
+        this.props.onBuscaRealizada(this.state.termoDeBusca)
+
+    }
+
     render() {
         return (
-            <div
-                className='flex flex-column'>
-                    <IconField iconPosition='left'>
+            <form onSubmit={this.onFormSubmit}>
+                <div
+                    className='flex flex-column'>
+                    <IconField  iconPosition='left'>
                         <InputIcon className='pi pi-search'></InputIcon>
-                        <InputText placeholder="Buscar algo..."/>
-                    </IconField> 
-            </div>
+                        <InputText 
+                            className='w-full' 
+                            placeholder={this.props.dica}
+                            onChange={this.onTermoAlterado}
+                            value={this.state.termoDeBusca}/>
+                    </IconField>
+                    <Button
+                        className='mt-3'
+                        label='OK'
+                        outlined />
+                </div>
+            </form>
         )
     }
 }
+
+Busca.defaultProps = {
+    dica: 'Buscar algo...'
+}
+
+
 
 export default Busca
