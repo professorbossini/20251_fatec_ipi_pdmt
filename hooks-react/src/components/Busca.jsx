3a92e4e7a47c34299eec79a981644cd1b0c94de7
Rodrigo Bossini <professorbossini@gmail.com>
2025-05-30 17:48:25 -0300

    learn(ap08-react): encerra a apostila
    - conteudo do semestre tambem finalizado
    - na semana que vem, teremos o inicio da prova

--- hooks-react/src/components/Busca.jsx
+++ hooks-react/src/components/Busca.jsx
@@ -1,12 +1,21 @@
 // rafce
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import striptags from 'striptags'
+import { Button } from 'primereact/button'
 import { IconField } from 'primereact/iconfield'
 import { InputIcon } from 'primereact/inputicon'
 import { InputText } from 'primereact/inputtext'
 const Busca = () => {
   const [termoDeBusca, setTermoDeBusca] = useState('')
   const [resultados, setResultados] = useState([])
+
+  // useEffect(() => {
+  //   console.log('Causando algum efeito colateral...')
+  //   return () => {
+  //     console.log("Fazendo uma limpeza qualquer, ou seja, desalocando recursos")
+  //   }
+  // })
 
   useEffect(() => {
     const buscar = async () => {
@@ -19,19 +28,49 @@ const Busca = () => {
           srsearch: termoDeBusca
         }
       })
-      setResultados(data)
+      setResultados(data.query.search)
     }
-    buscar()
+    const timeoutID = setTimeout(() => {
+      if(termoDeBusca)
+        buscar()
+    }, 1000)
+    return () => {
+      clearTimeout(timeoutID)
+    }
   }, [termoDeBusca])
 
   return (
-    <IconField iconPosition='left'>
-      <InputIcon className='pi pi-search'/>
-      <InputText 
-        placeholder='Buscar...'
-        onChange={(e) => {setTermoDeBusca(e.target.value)}}
-        value={termoDeBusca}/>
-    </IconField>
+    <div>
+      <IconField iconPosition='left'>
+        <InputIcon className='pi pi-search'/>
+        <InputText 
+          placeholder='Buscar...'
+          onChange={(e) => {setTermoDeBusca(e.target.value)}}
+          value={termoDeBusca}/>
+      </IconField>
+      {
+        resultados.map((resultado) =>(
+          <div
+            key={resultado.pageid}
+            className='my-3 border border-1 border-400'>
+              <div
+                className='border-bottom border border-1 border-400 p-3 text-center font-bold'>
+                {resultado.title}
+                <span>
+                  <Button 
+                    icon="pi pi-send"
+                    className='ml-3 p-button-rounded p-button-secondary'
+                    onClick={() => window.open(`https://en.wikipedia.org?curid=${resultado.pageid}`)}/>
+                </span>
+              </div>
+              <div
+                className='p-2'>
+                  {striptags(resultado.snippet)}
+              </div>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
